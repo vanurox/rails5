08e9678024a27b3e548ac45ad5eb9cd664e24872
vanurox <varunrocks27.sharma@gmail.com>
2018-01-05 12:23:54 +0530

    saved user and fetched all user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
   def index
     # render 'show'
-    redirect_to :controller=> "users", :action => "show"
+    # redirect_to :controller=> "users", :action => "show"
+    @users = User.all
+
+  end
+
+  def create
+    # puts params.inspect
+    # if params[:controller] == "users"
+    # end
+    # exit!
+    email = params[:email]
+    username = params[:username]
+    user = User.new
+    user.email = email
+    user.username = username
+    if user.save
+      @msg = "User Saved successfully!!"
+    else
+      @msg = "Error while saving user"
+    end
+    @users = User.all
+    # redirect_to :controller=> "users", :action => "index"
+        
+      render 'index'    
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
+  post "users/create"
+
+  root "users#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
